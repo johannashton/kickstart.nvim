1a75d89a1e29645d51f4c16dd96d23ff88d2610e
Johann Ashton <ashton.johann@gmail.com>
2025-09-01 03:20:34 +1000

    actual start

---
+++ init_old.lua
@@ -0,0 +1,6 @@
+--require("config.lazy")
+vim.wo.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4 -- Sets the visual width of a tab to 4 spaces
+vim.opt.shiftwidth = 4 -- Sets the indentation width to 4 spaces
+vim.g.have_nerd_font = true
