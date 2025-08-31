b356c179ea998439db93a51838b6cda5a18f9c79
Johann Ashton <ashton.johann@gmail.com>
2025-09-01 07:05:05 +1000

    added basedpyright

--- init.lua
+++ init.lua
@@ -674,6 +674,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
