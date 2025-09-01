2cc44bb4e9441a3903e45812180d294f0961cd61
Johann Ashton <ashton.johann@gmail.com>
2025-09-01 18:32:36 +1000

    pyright, venv-selector

--- init.lua
+++ init.lua
@@ -673,8 +673,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        basedpyright = {},
+        pyright = {},
+        --ruff = {},
+        --basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,22 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      --'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      'mfussenegger/nvim-dap-python', --optional
+      --{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    },
+    ft = 'python', -- Load when opening Python files
+    keys = {
+      { '<leader>vs', '<cmd>VenvSelect<cr>', desc = 'Select VirtualEnv' }, -- Open picker on keymap
+    },
+    opts = { -- this can be an empty lua table - just showing below for clarity.
+      search = {}, -- if you add your own searches, they go here.
+      options = {}, -- if you add plugin options, they go here.
+    },
+  },
+}
