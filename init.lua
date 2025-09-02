fd092e771c1b221ed8ad97c2ef564150d6c08ac0
Johann Ashton <ashton.johann@gmail.com>
2025-09-02 21:40:43 +1000

    folds

--- init.lua
+++ init.lua
@@ -104,6 +104,9 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+vim.opt.foldmethod = 'indent'
+vim.opt.foldlevelstart = 99
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
